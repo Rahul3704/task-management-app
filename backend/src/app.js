7968fa2787c206d5b0b866058f37588ea5818b7b
Rahul3704 <rahulsinhami5a@gmail.com>
2026-02-27 00:02:09 +0530

    Add route route for backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,9 @@ app.use(
     credentials: true
   })
 );
-
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "API is running" });
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
