e39d571bbb966a46f5c4651147331cc922e19712
Rahul3704 <rahulsinhami5a@gmail.com>
2026-02-27 14:46:47 +0530

    Fix CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,12 +12,11 @@ app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
